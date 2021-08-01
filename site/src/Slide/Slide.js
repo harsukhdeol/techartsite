9546c13fdeae1d3d0b2ba9ec3cbd990e06899911
Harsukh Deol <centaurgolden@gmail.com>
2021-08-01 01:45:12 -0400

    video plays!

--- site/src/Slide/Slide.js
+++ site/src/Slide/Slide.js
@@ -1,12 +1,18 @@
 import styles from "./Slide.module.css";
 import Page from "../Page/Page";
+import { useEffect, useRef } from "react";
+
 function Slide(data) {
+  const video = useRef(null);
+  useEffect(() => {
+    video.current.play();
+  });
   return (
     <div className={styles.Slide}>
       <div className={styles.header}>
         <div className={styles.background}>
-          <video width="320" height="240" autoplay>
-            <source src={data.video} type="video/mp4" />
+          <video ref={video} autoplay muted controls loop>
+            <source src={data.video} type="video/mp4"></source>
             Your browser does not support the video tag.
           </video>
           <h1 className={styles.text}>{data.text}</h1>
--- site/src/Slider/Slider.js
+++ site/src/Slider/Slider.js
@@ -1,23 +1,26 @@
+import React from "react";
 import { pages, menu } from "../data/data";
 import Slide from "../Slide/Slide";
 import MenuButton from "../MenuButton/MenuButton";
 import styles from "./Slider.module.css";
 
-function Slider() {
-  return (
-    <div className={styles.Slider}>
-      <div className={styles.slideContainer}>
-        {pages.map((page, i) => (
-          <Slide key={i} {...page} />
-        ))}
+class Slider extends React.Component {
+  render() {
+    return (
+      <div className={styles.Slider}>
+        <div className={styles.slideContainer}>
+          {pages.map((page, i) => (
+            <Slide key={i} {...page} />
+          ))}
+        </div>
+        <div className={styles.menuContainer}>
+          {menu.map((item, i) => (
+            <MenuButton {...item} />
+          ))}
+        </div>
       </div>
-      <div className={styles.menuContainer}>
-        {menu.map((item, i) => (
-          <MenuButton {...item} />
-        ))}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Slider;
