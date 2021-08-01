91161f69918128530712c95e29e85e1a5bdd3e51
Harsukh Deol <centaurgolden@gmail.com>
2021-08-01 18:54:29 -0400

    top looks ok

--- site/src/MenuButton/MenuButton.js
+++ site/src/MenuButton/MenuButton.js
@@ -2,14 +2,12 @@ import styles from "./MenuButton.module.css";
 
 export default function MenuButton({ data, index, onClick }) {
   return (
-    <div className={styles.MenuButton}>
-      <button onClick={() => onClick(index)}>
-        <h2 className={styles.text}>{data.text}</h2>
-        <div
-          className={styles.image}
-          style={{ backgroundImage: `url(${data.image})` }}
-        />
-      </button>
+    <div onClick={() => onClick(index)} className={styles.MenuButton}>
+      <h2 className={styles.text}>{data.text}</h2>
+      <div
+        className={styles.image}
+        style={{ backgroundImage: `url(${data.image})` }}
+      />
     </div>
   );
 }
